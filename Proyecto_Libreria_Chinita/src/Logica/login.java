906981658685a1d9420a8b26f7e85b29573ea22f
Josue Maldonado <Memejosue2012@hotmail.com>
2022-05-12 12:08:05 -0600

    Proyecto Terminado

---
+++ Proyecto_Libreria_Chinita/src/Logica/login.java
@@ -0,0 +1,70 @@
+
+package Logica;
+
+import java.sql.Date;
+
+public class login {
+    private int id;
+    private String nombre;
+    private String puesto;
+    private String usuario;
+    private String pass;
+
+    //constructor vacio
+    public login() {
+        
+    }
+
+    //constructor con datos
+    public login(int id, String nombre, String puesto,String usuario , String pass) {
+        this.id = id;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.usuario = usuario;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    
+    
+}
+
